9d2e373fc28182356073aded42ce923e79c9be41
mr.mk1950 <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-03-06 22:02:29 +0530

    c sharp

---
+++ find data value/Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Diagnostics;
+
+namespace find_data_value
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //user input number using int and show how much size store in memory 
+
+            Console.WriteLine("---------------- Integer Values ----------------");
+
+            Console.WriteLine("Integer value : ");
+            int a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Value: {a}, Size: {sizeof(int)} bytes");
+
+            //show the min and max value
+            Console.WriteLine("Int MinValue : " + int.MinValue);
+            Console.WriteLine("Int MaxValue : " + int.MaxValue);
+
+            Console.WriteLine();
+            Console.WriteLine("--------------- uint Value ---------------------");
+            //user enter value and show the size of value
+            Console.WriteLine("Please Enter the number : ");
+            uint ui = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine($"Value: {ui}, Size: {sizeof(uint)} uint.");
+
+            // show the min and max value
+            Console.WriteLine("uint MinValue : " + uint.MinValue);
+            Console.WriteLine("uint MaxValue : " + uint.MaxValue);
+
+            Console.WriteLine();
+            Console.WriteLine("------------- SByte Values ------------------");
+
+            //user enter value and show the size of value 
+            Console.WriteLine("Please Enter the number : ");
+            SByte sb = Convert.ToSByte(Console.ReadLine());
+            Console.WriteLine($"Value: {sb}, Size: {sizeof(sbyte)} bytes.");
+
+            //show the min and max value
+            Console.WriteLine("Sbyte MinValue : " + sbyte.MinValue);
+            Console.WriteLine("Sbyte MaxValue : " + sbyte.MaxValue);
+
+            Console.WriteLine();
+            Console.WriteLine("----------------- Byte Values ------------------");
+
+            //user enter value and show the size of value
+            Console.WriteLine("Please Enter the number : ");
+            Byte bt = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine($"Value: {bt}, Size: {sizeof(byte)} bytes.");
+
+            // show the min and max value
+            Console.WriteLine("byte MinValue : " + byte.MinValue);
+            Console.WriteLine("byte MaxValue : " + byte.MaxValue);
+
+            Console.WriteLine();
+            Console.WriteLine("--------------- short Value ---------------------");
+
+            //user enter value and show the size of value
+            Console.WriteLine("Please Enter the number : ");
+            short sh = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine($"Value: {sh}, Size: {sizeof(short)} short.");
+
+            // show the min and max value
+            Console.WriteLine("short MinValue : " + short.MinValue);
+            Console.WriteLine("short MaxValue : " + short.MaxValue);
+
+            Console.WriteLine();
+            Console.WriteLine("--------------- Unshort Value ---------------------");
+
+            //user enter value and show the size of value
+            Console.WriteLine("Please Enter the number : ");
+            ushort ush = Convert.ToUInt16(Console.ReadLine());
+            Console.WriteLine($"Value: {ush}, Size: {sizeof(ushort)} ushort.");
+
+            // show the min and max value
+            Console.WriteLine("ushort MinValue : " + ushort.MinValue);
+            Console.WriteLine("ushort MaxValue : " + ushort.MaxValue);
+
+            Console.WriteLine();
+            Console.WriteLine("--------------- long Value ---------------------");
+
+            //user enter value and show the size of value
+            Console.WriteLine("Please Enter the number : ");
+            long ln = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine($"Value: {ln}, Size: {sizeof(long)} long.");
+
+            // show the min and max value
+            Console.WriteLine("long MinValue : " + long.MinValue);
+            Console.WriteLine("long MaxValue : " + long.MaxValue);
+
+            Console.WriteLine();
+            Console.WriteLine("--------------- ulong Value ---------------------");
+
+            //user enter value and show the size of value
+            Console.WriteLine("Please Enter the number : ");
+            ulong uln = Convert.ToUInt64(Console.ReadLine());
+            Console.WriteLine($"Value: {uln}, Size: {sizeof(ulong)} ulong.");
+
+            // show the min and max value
+            Console.WriteLine("ulong MinValue : " + ulong.MinValue);
+            Console.WriteLine("ulong MaxValue : " + ulong.MaxValue);
+
+            Console.WriteLine();
+            Console.WriteLine("--------------- char Value ---------------------");
+
+            //user enter value and show the size of value
+            Console.WriteLine("Please Enter any one character : ");
+            char ch = Console.ReadKey().KeyChar;
+            Console.WriteLine($"Value: {ch}, Size: {sizeof(char)} char.");
+
+            // Show the min and max value
+            Console.WriteLine($"char MinValue : {char.MinValue}");
+            Console.WriteLine($"char MaxValue : {char.MaxValue}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ index popup/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class form : Form
+    {
+        List<string> li = new List<string>()
+        {
+            "-- Select --",
+            "India",
+            "Bharat",
+            "Hindustan",
+        };
+        public form()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void form_Load_1(object sender, EventArgs e)
+        {
+            comboBox1.DataSource = li;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // selectedvalue ni badle selectindex karvathi number aavse.
+                string m = comboBox1.SelectedIndex.ToString();
+             //   MessageBox.Show(m);
+            if (m.Equals("0")) 
+            {
+                MessageBox.Show("please select your country", "warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                MessageBox.Show(comboBox1.SelectedValue.ToString());
+            }
+        }
+    }
+}
+
