f47a46f628c190b28f92f5ceac6bf0912f049772
mr.mk1950 <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-03-06 22:09:31 +0530

    c sharp

---
+++ for each loop/for each loop/for each loop/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+char[] message = { 'I', 'n', 'd', 'i', 'a' };
+    foreach (char i in message)
+        Console.WriteLine(i);
+    Console.ReadLine();
