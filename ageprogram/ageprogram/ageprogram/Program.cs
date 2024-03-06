4cd1ee9b45b1a370d79c8d81ecfb19b03d6647ae
mr.mk1950 <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-03-06 22:10:55 +0530

    c sharp

---
+++ ageprogram/ageprogram/ageprogram/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ageprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.Write("Enter your age:");
+            int age = Convert.ToInt32(Console.Read());
+            if (age > 0)
+            {
+                if (age >= 18 && age < 60)
+                {
+                    Console.Write("Adult");
+
+                }
+                else if (age < 18)
+                {
+                    Console.Write("Teenage");
+                }
+                else if (age >= 60)
+                {
+                    Console.Write("Senior citizen");
+                }
+            }
+            else
+            {
+                Console.Write("Birth Pending");
+            }
+            Console.ReadKey();
+        }
+    }
+}
