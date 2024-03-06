fd8268a9c17cca80a7cadf7c6f1b2ac6d998e42f
mr.mk1950 <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-03-06 22:05:38 +0530

    c sharp

---
+++ while loop/while loop/while loop/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int counter = 5;
+while (counter>0)
+{
+    Console.WriteLine("counter = {0}", counter);
+    counter=counter-1;
+    Console.ReadKey();
+}    
