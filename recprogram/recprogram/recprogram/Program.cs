0dc09967ad586f4e1210c95256e0492498c85d93
mr.mk1950 <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-03-06 22:07:09 +0530

    c sharp

---
+++ recprogram/recprogram/recprogram/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace recprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,] b = new int[3, 3, 3];
+            b[0, 0, 0] = 1;
+            b[0, 0, 1] = 2;
+            b[0, 0, 2] = 3;
+            b[0, 1, 0] = 4;
+            b[0, 1, 1] = 5;
+            b[0, 1, 2] = 6;
+            foreach (int c in b)
+            {
+                Console.Write(c);
+            }
+            Console.Read();
+        }
+    }
+}
