78089fb67b5f1f1eb88fadf89552a8d0e1e08e86
mr.mk1950 <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-03-06 22:22:58 +0530

    c sharp

---
+++ array/array/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace programarray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[3] { 20, 32, 12 };
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
