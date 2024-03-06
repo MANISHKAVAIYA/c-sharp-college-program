84e698f428f4e2e42fb477c1a366abe820dee49f
mr.mk1950 <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-03-06 22:06:17 +0530

    c sharp

---
+++ switch statement/switch statement/switch statement/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Contracts;
+Console.Write("Enter your grade: ");
+string userGrade =  Console.ReadLine();
+
+switch (userGrade)
+{
+    case "A+":
+    case "A":
+        Console.WriteLine("Distinction");
+        break;
+    case "B":
+        Console.WriteLine("B Grade");
+        break;
+    case "C":
+        Console.WriteLine("C Grade");
+        break; 
+    default:
+        Console.WriteLine("Fail");
+        break;
+}
