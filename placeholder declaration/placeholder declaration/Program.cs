fd75ccd1b245af7211ec6554ca37347234927db1
mr.mk1950 <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-03-06 22:20:22 +0530

    c sharp

---
+++ placeholder declaration/placeholder declaration/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace placeholder_declaration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Name : ");
+            string name=Console.ReadLine();
+            Console.WriteLine("Your name is : {0} ", name);
+            Console.ReadLine();
+        }
+    }
+}
