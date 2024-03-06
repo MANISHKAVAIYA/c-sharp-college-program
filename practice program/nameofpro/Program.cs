800ecac56d3b79d6d18985dbc1c05f7eec754714
mr.mk1950 <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-03-06 22:32:59 +0530

    c sharp

---
+++ practice program/nameofpro/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace nameofpro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double setup = 23.2;
+            Console.WriteLine($"The variable {nameof(setup)} has the value{setup}.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ practice program/practice program/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Reflection;
+namespace practice_program
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly? myApp = Assembly.GetEntryAssembly();
+            if (myApp is null) return;
+            foreach (AssemblyName name in myApp.GetReferencedAssemblies())
+            {
+                Assembly a = Assembly.Load(name);
+                int methodCount = 0;
+                foreach (TypeInfo t in a.DefinedTypes)
+                {
+                    methodCount += t.GetMethods().Length;
+                }
+                Console.WriteLine("{0:NO} type with {1:NO} methods in {2} assembly.",
+                    arg0: a.DefinedTypes.Count(),
+                    arg1: methodCount,
+                    arg2:name.Name);
+                Console.WriteLine("Hello World!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
