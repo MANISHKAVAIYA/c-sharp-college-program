5287675573de5611f74cfe507fbda7f4305f3ff7
mr.mk1950 <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-03-08 23:08:16 +0530

    c sharp

---
+++ show alphabet/show alphabet/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace show_alphabet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any number (1-26) : ");
+            int a=Convert.ToInt32(Console.ReadLine());
+
+            if (a>=1 && a<=26)
+            {
+                char alphabet = (char)('a' + a - 1);
+                Console.WriteLine($"The alphabet for {a} is: {alphabet}");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid integer between 1 and 26.");
+            }
+            Console.Read();
+        }
+    }
+}
