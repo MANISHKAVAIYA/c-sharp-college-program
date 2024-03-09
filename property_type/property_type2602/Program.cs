7bf6e3afa2a5cc0d9941e7e41e544e4a2fe84acc
mr.mk1950 <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-03-09 06:53:18 +0530

    c sharp

---
+++ property_type/property_type2602/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+
+namespace property_type2602
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rectangle = new Rectangle();
+            if (rectangle is { Height : 9 } )
+            {
+                Console.WriteLine("The rectangle's height is 0, it will look like a sleeping line.");
+            }
+            else
+            {
+                Console.WriteLine("The rectangle's height is not zero, it will look like a not sleeping line.");
+            }
+            Console.Read();
+        }
+    }
+}
