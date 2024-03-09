9fede19971e3229a95d6d8a081e590444fe69ed4
mr.mk1950 <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-03-09 06:51:23 +0530

    c sharp

---
+++ constructor with para/constructor with para/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Cryptography;
+
+namespace constructor_with_para
+{
+    public class conswith
+    {
+        public int number1, number2;
+
+        public conswith(int num1, int num2)
+        {
+            number1 = num1;
+            number2 = num2;
+        }
+        public static conswith operator -(conswith c1)
+        {
+            c1.number1 = -c1.number1;
+            c1.number2 = -c1.number2;
+            return c1;
+        }
+        public void print()
+        {
+            Console.WriteLine(number1);
+            Console.WriteLine(number2);
+        }
+    }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            conswith c1 = new conswith(25, -100);
+            c1 = -c1;
+            c1.print();
+            Console.Read();
+        }
+    }
+}
