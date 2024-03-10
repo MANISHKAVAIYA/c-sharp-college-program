1388ce92cda324d5ce075cc626e148c77e426f74
mr.mk1950 <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-03-10 20:18:57 +0530

    c sharp

---
+++ class demo1/class demo1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace class_demo1
+{
+    public class A
+    {
+        public void print()
+        {
+            Console.WriteLine("This super class.");
+        }
+    }
+    public class B: A 
+    { 
+        public void Print()
+        {
+            
+            Console.WriteLine("This is sub class");
+        }   
+    }
+}
