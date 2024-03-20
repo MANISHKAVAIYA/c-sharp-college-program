c21f24d51f0f3eca4efe1cd1ea0fe3f7c8109645
mr.mk1950 <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-03-20 09:49:51 +0530

    c sharp

---
+++ break statement inner loop/inner loop using break.cs
@@ -0,0 +1,43 @@
+﻿/**
+ * This program basically user input value
+ * Main : user input value and which number of stop the print user control start and stop.
+ * author = Manish Kavaiya
+ * **/
+
+namespace break_statement_inner_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int txt1,txt2;
+
+            Console.WriteLine("Enter number : ");
+            txt1=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter your break number : ");
+            txt2 = Convert.ToInt32(Console.ReadLine());
+
+            if (txt1>200)
+            {
+                Console.WriteLine("Please enter value less than 200");
+            }
+            else
+            {
+                for (int i = 1; i <= txt1; i++)
+                {
+
+                    for (int j = 0; j <= txt2; j++)
+                    {
+                        if (j == txt2)
+                            break;
+
+                        Console.WriteLine(i + " " + j);
+                    }
+                }
+            }
+            Console.WriteLine("Loop stopped.....please enter any key.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ continue statement/continue_state.cs
@@ -0,0 +1,52 @@
+﻿/**
+ * This program basically user input value
+ * This program is continue statement
+ * author = Manish Kavaiya
+ * **/
+
+namespace continue_statement
+{
+    internal class continue_state
+    {
+        static void Main(string[] args)
+        {
+            l1:
+            Console.WriteLine("please must be every value less than 500");
+
+            Console.WriteLine("Please enter your start number : ");
+            int convalue1=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter your continue number : ");
+            int convalue2=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("please enter your skip number");
+            int skipvalue1=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter your skip number 2");
+            int skipvalue2=Convert.ToInt32(Console.ReadLine());
+
+            //loop initilize
+            if (convalue1 > 500 && convalue2 > 500)
+            {
+                Console.WriteLine("Your value is not less than 500.");
+                Console.WriteLine("please enter only value that is less than 500.");
+                goto l1;
+            }
+            else
+            {
+                for (int i = 1; i <= skipvalue1; i++)
+                {
+                    for(int j = 1; j <= skipvalue2; j++)
+                    {
+                        if(i == convalue1 && j == convalue2)
+                        {
+                            continue;
+                        }
+                        Console.WriteLine(i + " " + j);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
